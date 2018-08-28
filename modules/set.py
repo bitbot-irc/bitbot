b2259c2ea85b44b4b83619d8f4a440ca29972b9e
jesopo <github@lolnerd.net>
2018-08-28 14:55:57 +0100

    Added !channelsetoverride in set.py

--- modules/set.py
+++ modules/set.py
@@ -9,16 +9,22 @@ class Module(object):
             self.postboot_set, replay=True)
         bot.events.on("postboot").on("configure").on("channelset"
             ).hook(self.postboot_channelset, replay=True)
+
         bot.events.on("received").on("command").on("set").hook(
             self.set, help="Set a specified user setting",
             usage="<setting> <value>")
         bot.events.on("received").on("command").on("get").hook(
             self.get, help="Get a specified user setting",
             usage="<setting>", min_args=1)
+
         bot.events.on("received").on("command").on("channelset"
             ).hook(self.channel_set, channel_only=True,
             help="Set a specified setting for the current channel",
             usage="<setting> <value>", require_mode="o")
+        bot.events.on("received").on("command").on("channelsetoverride"
+            ).hook(self.channel_set, channel_only=True,
+            help="Set a specified setting for the current channel",
+            usage="<setting> <value>", permission="channelsetoverride")
         bot.events.on("received").on("command").on("channelget"
             ).hook(self.channel_get, channel_only=True,
             help="Get a specified setting for the current channel",
