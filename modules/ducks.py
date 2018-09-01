2db5d476a457c80dc767b59e710fedfc73ab459a
dngfx <294904+dngfx@users.noreply.github.com>
2018-09-01 20:54:18 +0100

    Uncomment duck stats

--- modules/ducks.py
+++ modules/ducks.py
@@ -28,8 +28,9 @@ class Module(object):
                                                    help="See who the friendliest people to ducks are!")
         events.on("received.command.killers").hook(self.duck_enemies,
                                                      help="See who shoots the most amount of ducks.")
-        # events.on("received.command.ducks").hook(self.duck_list,
-        #                                              help="Shows a list of the most popular duck superstars.")
+        events.on("received.command.ducks").hook(self.duck_stats,
+                                                      help="Shows your duck "
+                                                           "stats!")
 
 
         events.on("postboot").on("configure").on(
