8959b6cfd24233c07f5a3195d5ae09b866f924d0
jesopo <github@lolnerd.net>
2018-09-09 10:17:22 +0100

    Utilise !serverset in more modules

--- modules/accept_invite.py
+++ modules/accept_invite.py
@@ -1,10 +1,14 @@
-
+import Utils
 
 class Module(object):
     def __init__(self, bot, events, exports):
         events.on("received").on("invite").hook(self.on_invite)
+        exports.add("serverset", {"setting": "accept-invites",
+            "help": "Set whether I accept invites on this server",
+            "validate": Utils.bool_or_none})
 
     def on_invite(self, event):
         if event["server"].is_own_nickname(event["target_user"].nickname):
             if event["server"].get_setting("accept-invites", True):
                 event["server"].send_join(event["target_channel"])
+
