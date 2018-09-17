3576f18a82d1a1fb4dfe8b4fe85fc6bbc2ebce87
jesopo <github@lolnerd.net>
2018-09-17 10:47:08 +0100

    Verify server TLS certificates

--- IRCServer.py
+++ IRCServer.py
@@ -85,6 +85,8 @@ class Server(object):
         context.options |= ssl.OP_NO_SSLv2
         context.options |= ssl.OP_NO_SSLv3
         context.options |= ssl.OP_NO_TLSv1
+        context.load_default_certs()
+        context.verify_mode = ssl.CERT_REQUIRED
         self.socket = context.wrap_socket(self.socket)
 
     def connect(self):
