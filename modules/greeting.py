6274159ac699f063eec258587c56865d476d0086
jesopo <github@lolnerd.net>
2018-09-27 10:27:19 +0100

    Fix event typo in greeting.py ("recevied" -> "received")

--- modules/greeting.py
+++ modules/greeting.py
@@ -5,7 +5,7 @@ class Module(object):
         exports.add("channelset", {"setting": "greeting",
             "help": "Set a greeting to send to users when they join"})
 
-    @Utils.hook("recevied.join")
+    @Utils.hook("received.join")
     def join(self, event):
         greeting = event["channel"].get_setting("greeting", None)
         if greeting:
