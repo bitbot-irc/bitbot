27c191a668417d9b2d43012ee17c6fc6d0fa40ce
jesopo <github@lolnerd.net>
2018-10-03 16:00:07 +0100

    Remove unneeded \n

--- modules/nickserv.py
+++ modules/nickserv.py
@@ -6,8 +6,7 @@ from src import EventManager, ModuleManager, utils
 class Module(ModuleManager.BaseModule):
     @utils.hook("received.numeric.001", priority=EventManager.PRIORITY_URGENT)
     def on_connect(self, event):
-        nickserv_password = event["server"].get_setting(
-            "nickserv-password")
+        nickserv_password = event["server"].get_setting("nickserv-password")
         if nickserv_password:
             event["server"].attempted_auth = True
             event["server"].send_message("nickserv",
