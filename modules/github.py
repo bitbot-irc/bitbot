1fa66eebc6c1373fb2a04d300a1bb2e069aa04db
jesopo <github@lolnerd.net>
2018-10-06 10:02:02 +0100

    Attempt to register servers for read/write when sending github hook notices

--- modules/github.py
+++ modules/github.py
@@ -37,3 +37,4 @@ class Module(ModuleManager.BaseModule):
 
                     self.events.on("send.stdout").call(target=channel,
                         module_name="Github", server=server, message=line)
+                    self.bot.register_both(server)
