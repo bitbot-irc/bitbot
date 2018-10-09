88e523c3deaf6d4b95ad46a072a5f61211e5a87c
jesopo <github@lolnerd.net>
2018-10-09 22:15:27 +0100

    Add modules/eval_lua.py

---
+++ modules/eval_lua.py
@@ -0,0 +1,26 @@
+import socket
+from src import ModuleManager, utils
+
+EVAL_URL = "https://www.lua.org/cgi-bin/demo"
+
+class Module(ModuleManager.BaseModule):
+    _name = "Lua"
+    @utils.hook("received.command.lua", min_args=1)
+    def eval(self, event):
+        try:
+            page = utils.http.get_url(EVAL_URL,
+                post_params={"input": event["args"]},
+                method="POST",
+                soup=True)
+        except socket.timeout:
+            event["stderr"].write("%s: eval timed out" %
+                event["user"].nickname)
+            return
+
+        if page:
+            textareas = page.find_all("textarea")
+            if len(textareas) > 1:
+                out = textareas[1].text.strip()
+                event["stdout"].write("%s: %s" % (event["user"].nickname, out))
+        else:
+            event["stderr"].write("%s: failed to eval" % event["user"].nickname)
