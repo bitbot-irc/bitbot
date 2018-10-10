0b44788ac5a441caea3fac926a3c3f41e65d05d9
jesopo <github@lolnerd.net>
2018-10-10 12:10:53 +0100

    Support coin bets without a whole number (e.g. '0.1' can be '.1')

--- modules/coins.py
+++ modules/coins.py
@@ -7,7 +7,7 @@ DEFAULT_REDEEM_AMOUNT = "100.0"
 DEFAULT_INTEREST_RATE = "0.01"
 INTEREST_INTERVAL = 60*60 # 1 hour
 DECIMAL_ZERO = decimal.Decimal("0")
-REGEX_FLOAT = re.compile("\d+(?:\.\d{1,2}|$)")
+REGEX_FLOAT = re.compile("(?:\d+(?:\.\d{1,2}|$)|\.\d{1,2})")
 
 RED = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 BLACK = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
