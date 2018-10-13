b2f948a73b4137c3b4fa3a80764ae1702b7aaa2d
jesopo <github@lolnerd.net>
2018-10-13 16:47:46 +0100

    Add !totalcoins in modules/coins.py, to check what the total coins in circulation is

--- modules/coins.py
+++ modules/coins.py
@@ -64,6 +64,18 @@ class Module(ModuleManager.BaseModule):
         event["stdout"].write("The Bank has %s coins" %
             "{0:.2f}".format(self._get_pool(event["server"])))
 
+    @utils.hook("received.command.totalcoins")
+    def total_coins(self, event):
+        all_coins = event["server"].get_all_user_settings("coins", [])
+        all_coins = list(filter(lambda coin: decimal.Decimal(coin[1]),
+            all_coins))
+        all_coins = [decimal.Decimal(coin[1]) for coin in all_coins]
+        all_coins = sum(all_coins)
+        pool = self._get_pool(event["server"])
+
+        event["stdout"].write("Total coins: %s" % "{0:.2f}".format(
+            pool+all_coins))
+
     @utils.hook("received.command.coins")
     def coins(self, event):
         """
