f08624ddbdcdf33104e6cc037f84c78f77199c9d
jesopo <github@lolnerd.net>
2018-10-17 15:04:38 +0100

    Escape \n in EVAL_TEMPLATE in modules/eval_python.py

--- modules/eval_python.py
+++ modules/eval_python.py
@@ -16,9 +16,9 @@ except Exception as e:
     old_stdout.write(json.dumps({"success": False, "out": str(e)}))
     sys.exit()
 
-stdout.write("\n")
+stdout.write("\\n")
 if not result == None:
-    stdout.write(str(result)+"\n")
+    stdout.write(str(result)+"\\n")
 old_stdout.write(json.dumps({"success": True, "out": stdout.getvalue()}))
 """
 
