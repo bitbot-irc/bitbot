9054ee7cd248bf96169238e74db028895126d986
jesopo <github@lolnerd.net>
2018-11-17 12:25:14 +0000

    How hard is it to refactor a dice module

--- modules/dice.py
+++ modules/dice.py
@@ -25,5 +25,5 @@ class Module(ModuleManager.BaseModule):
         if len(results) > 1:
             total = " (total: %d)" % total_n
 
-        event["stdout"].write("Rolled %s and got %s%s" % (
+        event["stdout"].write("Rolled %s and got %d%s" % (
             roll, total_n, ", ".join(str(r) for r in results)))
