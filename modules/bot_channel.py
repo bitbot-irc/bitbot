0e5daae70731f47895dc1dc62e98d45e27a321dd
Sapein <sapeint@hashbang.sh>
2018-11-30 20:58:10 -0500

    Add in bot channel configuration in bot.config

--- modules/bot_channel.py
+++ modules/bot_channel.py
@@ -5,5 +5,9 @@ from src import ModuleManager, utils
 class Module(ModuleManager.BaseModule):
     @utils.hook("received.numeric.001")
     def do_join(self, event):
-        event["server"].send_join(event["server"].get_setting("bot-channel",
-            "#bitbot"))
+        try:
+            event["server"].send_join(event["server"].get_setting("bot-channel",
+                "#bitbot"))
+        except KeyError:
+            event["server"].send_join(event["server"].get_setting("bot-channel",
+                self.bot.config['bot-channel']))
