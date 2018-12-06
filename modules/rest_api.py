9918a72f80d2fb529ab6789a4651273680eff52c
jesopo <github@lolnerd.net>
2018-12-06 12:03:58 +0000

    .items() on self.headers, to correctly use it for a dict comprehension (rest_api.py)

--- modules/rest_api.py
+++ modules/rest_api.py
@@ -14,7 +14,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
         _, _, endpoint = path[1:].partition("/")
         endpoint, _, args = endpoint.partition("/")
         args = list(filter(None, args.split("/")))
-        headers = {key.title(): value for key, value in self.headers}
+        headers = {key.title(): value for key, value in self.headers.items()}
 
         response = ""
         code = 404
