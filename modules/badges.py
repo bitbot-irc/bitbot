24ea68d150bdd9fbe588e0dee8d77c4bf5cd3659
jesopo <github@lolnerd.net>
2018-12-20 20:49:15 +0000

    Tell a user when a badge was successfully updated (badges.py)

--- modules/badges.py
+++ modules/badges.py
@@ -119,3 +119,4 @@ class Module(ModuleManager.BaseModule):
 
         badges[found_badge] = self._format_datetime(value)
         self._set_badges(event["user"], badges)
+        event["stdout"].write("Updated '%s' badge")
