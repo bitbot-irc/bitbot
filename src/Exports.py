8c1c10b3c7aa9edde8b4ca98ca025a66ba371344
jesopo <github@lolnerd.net>
2019-02-07 10:02:27 +0000

    Implement `Exports.get_one()`

--- src/Exports.py
+++ src/Exports.py
@@ -29,6 +29,9 @@ class Exports(object):
         return self._exports.get(setting, []) + sum([
             exports.get(setting, []) for exports in
             self._context_exports.values()], [])
+    def get_one(self, setting: str) -> typing.Optional[typing.Any]:
+        values = self.get_all(setting)
+        return values[0] if values else None
 
     def purge_context(self, context: str):
         if context in self._context_exports:
