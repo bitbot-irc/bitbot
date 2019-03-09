2d949391a6aea44f1109310cf0ab0cf450c43247
jesopo <github@lolnerd.net>
2019-03-09 13:37:59 +0000

    Support setting strip-color per-channel not just per-server

--- modules/strip_color.py
+++ modules/strip_color.py
@@ -3,11 +3,16 @@ from src import ModuleManager, utils
 @utils.export("serverset", {"setting": "strip-color",
     "help": "Set whether I strip colors from my messages on this server",
     "validate": utils.bool_or_none})
+@utils.export("channelset", {"setting": "strip-color",
+    "help": "Set whether I strip colors from my messages on in this channel",
+    "validate": utils.bool_or_none})
 class Module(ModuleManager.BaseModule):
     @utils.hook("preprocess.send.privmsg")
     @utils.hook("preprocess.send.notice")
     def preprocess(self, event):
-        if event["server"].get_setting("strip-color", False):
+        strip_color = event["channel"].get_setting("strip-color",
+            event["server"].get_setting("strip-color", False))
+        if strip_color:
             message = event["line"].args.get(-1)
             if not message == None:
                 event["line"].args[-1] = utils.irc.strip_font(message)
