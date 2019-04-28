a7a3f5fd2553243d5f93f84da5056e0bc5c2ff0c
jesopo <github@lolnerd.net>
2019-04-28 15:20:37 +0100

    Remove incorrect and redundant `usage` !weather

--- modules/weather.py
+++ modules/weather.py
@@ -10,7 +10,7 @@ class Module(ModuleManager.BaseModule):
         if not user_location == None:
             return "%s, %s" % (user_location["city"], user_location["country"])
 
-    @utils.hook("received.command.weather", usage="<location>")
+    @utils.hook("received.command.weather")
     def weather(self, event):
         """
         :help: Get current weather for you or someone else
