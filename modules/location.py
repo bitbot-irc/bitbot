4466bc34de3d82ae95fc726afa89635018607fb3
jesopo <github@lolnerd.net>
2019-04-28 10:50:35 +0100

    Add location.py, to allow users to set their location

---
+++ modules/location.py
@@ -0,0 +1,17 @@
+from src import ModuleManager, utils
+import pytz
+
+_lower_timezones = {}
+for tz in pytz.all_timezones:
+    if "/" in tz:
+        _lower_timezones[tz.split("/", 1)[1].lower()] = tz
+    _lower_timezones[tz.lower()] = tz
+
+def _find_tz(s):
+    print(_lower_timezones.get(s.lower(), None))
+    return _lower_timezones.get(s.lower(), None)
+
+@utils.export("set", {"setting": "location", "help": "Set your location",
+    "validate": _find_tz})
+class Module(ModuleManager.BaseModule):
+    pass
