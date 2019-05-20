281e14d1fbfeb48871adb3d33c943e69bb170cb8
jesopo <github@lolnerd.net>
2019-05-20 16:36:17 +0100

    Add the basics of usig BRB instead of QUIT for draft/resume-0.4

--- modules/resume.py
+++ modules/resume.py
@@ -73,3 +73,9 @@ class Module(ModuleManager.BaseModule):
         if not channel_name in resume_channels:
             resume_channels.append(channel_name)
             event["server"].set_setting("resume-channels", resume_channels)
+
+    @utils.hook("preprocess.send.quit")
+    def preprocess_send(self, event):
+        if event["line"].command == "QUIT" and event["server"].has_capability(
+                CAP):
+            event["line"].command = "BRB"
