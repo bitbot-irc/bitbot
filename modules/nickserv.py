f0230eb00194ba343c7de4580be29bff3978fa11
jesopo <github@lolnerd.net>
2019-05-23 16:04:09 +0100

    `attempted_auth` is no longer used

--- modules/nickserv.py
+++ modules/nickserv.py
@@ -9,6 +9,5 @@ class Module(ModuleManager.BaseModule):
     def on_connect(self, event):
         nickserv_password = event["server"].get_setting("nickserv-password")
         if nickserv_password:
-            event["server"].attempted_auth = True
             event["server"].send_message("nickserv",
                 "identify %s" % nickserv_password)
