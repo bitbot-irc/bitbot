248772f3e3b4a7cce0428ef30fa2a96e906778c7
jesopo <github@lolnerd.net>
2019-06-03 12:55:09 +0100

    Bump resume back down to `draft/resume-0.4` (from `draft/resume-0.5`) for now

--- modules/ircv3_resume.py
+++ modules/ircv3_resume.py
@@ -2,7 +2,7 @@
 
 from src import ModuleManager, utils
 
-CAP = utils.irc.Capability(None, "draft/resume-0.5")
+CAP = utils.irc.Capability(None, "draft/resume-0.4")
 
 class Module(ModuleManager.BaseModule):
     def _setting(self, new):
