11643f6eea28df901983f9c3249542f424daf608
jesopo <github@lolnerd.net>
2019-06-13 11:54:13 +0100

    Expose IRCChannel.mode_str() on REST API stats endpoint

--- modules/stats.py
+++ modules/stats.py
@@ -96,7 +96,8 @@ class Module(ModuleManager.BaseModule):
                 key=lambda nickname: nickname.lower()),
             "topic": channel.topic,
             "topic-set-at": channel.topic_time,
-            "topic-set-by": channel.topic_setter_nickname
+            "topic-set-by": channel.topic_setter_nickname,
+            "modes": channel.mode_str()
         }
     @utils.hook("api.get.channels")
     def channels_api(self, event):
