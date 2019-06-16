5b58cc05b64424f50d98d63ab7a026b4100b7693
jesopo <github@lolnerd.net>
2019-06-16 09:52:04 +0100

    Check that we have enough PRIVMSG args for strip_color.py

--- modules/strip_color.py
+++ modules/strip_color.py
@@ -12,13 +12,13 @@ class Module(ModuleManager.BaseModule):
     @utils.hook("preprocess.send.privmsg")
     @utils.hook("preprocess.send.notice")
     def preprocess(self, event):
-        strip_color = event["server"].get_setting("strip-color", False)
-        target = event["line"].args[0]
-        if not strip_color and target in event["server"].channels:
-            channel = event["server"].channels.get(target)
-            strip_color = channel.get_setting("strip-color", False)
+        if len(event["line"].args) > 1:
+            strip_color = event["server"].get_setting("strip-color", False)
+            target = event["line"].args[0]
+            if not strip_color and target in event["server"].channels:
+                channel = event["server"].channels.get(target)
+                strip_color = channel.get_setting("strip-color", False)
 
-        if strip_color:
-            message = event["line"].args.get(-1)
-            if not message == None:
-                event["line"].args[-1] = utils.irc.strip_font(message)
+            if strip_color:
+                message = event["line"].args[1]
+                event["line"].args[1] = utils.irc.strip_font(message)
