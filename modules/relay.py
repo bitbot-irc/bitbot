afa6b86c41d18afd1b9ceb4c8b0192f8fc97a77c
jesopo <github@lolnerd.net>
2019-06-20 20:11:40 +0100

    use event["from_self"] instead of checking nickname

--- modules/relay.py
+++ modules/relay.py
@@ -5,7 +5,7 @@ class Module(ModuleManager.BaseModule):
         return channel.get_setting("channel-relays", [])
 
     def _relay(self, event, channel):
-        if event["server"].is_own_nickname(event["user"].nickname):
+        if event["from_self"]:
             return
 
         relays = self._get_relays(channel)
