60a4f7aa4a467e62200b8ab34ab20041c850d168
jesopo <github@lolnerd.net>
2019-06-24 15:54:29 +0100

    Add missing colors.py file

---
+++ modules/git_webhooks/colors.py
@@ -0,0 +1,6 @@
+COLOR_BRANCH = utils.consts.ORANGE
+COLOR_REPO = utils.consts.GREY
+COLOR_POSITIVE = utils.consts.GREEN
+COLOR_NEUTRAL = utils.consts.LIGHTGREY
+COLOR_NEGATIVE = utils.consts.RED
+COLOR_ID = utils.consts.PINK
