55e5693341d003e07866dd73a8664a6996347c76
jesopo <github@lolnerd.net>
2019-07-09 21:25:43 +0100

    ircv3_botignore.py should use HIGH priority

--- modules/ircv3_botignore.py
+++ modules/ircv3_botignore.py
@@ -1,10 +1,11 @@
-from src import ModuleManager, utils
+from src import EventManager, ModuleManager, utils
 
 TAG = utils.irc.MessageTag(None, "inspircd.org/bot")
 
 class Module(ModuleManager.BaseModule):
     @utils.hook("received.message.private")
     @utils.hook("received.message.channel")
+    @utils.kwarg("priority", EventManager.PRIORITY_HIGH)
     def message(self, event):
         if TAG.present(event["tags"]):
             event.eat()
