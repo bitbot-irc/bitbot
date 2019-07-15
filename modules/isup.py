5be7626fb463d98aa3f4d466ebf13310c86ffb05
jesopo <github@lolnerd.net>
2019-07-15 14:59:37 +0100

    add isup.py

---
+++ modules/isup.py
@@ -0,0 +1,18 @@
+from src import ModuleManager, utils
+
+class Module(ModuleManager.BaseModule):
+    @utils.hook("received.command.isup")
+    @utils.kwarg("min_args", 1)
+    @utils.kwarg("help", "Check if a given URL is up or not")
+    @utils.kwarg("usage", "<url>")
+    def isup(self, event):
+        url = event["args_split"][0]
+
+        response = None
+        try:
+            response = utils.http.request(url)
+        except:
+            raise utils.EventError("%s looks down to me" % url)
+
+        event["stdout"].write("%s looks up to me (HTTP %d)" %
+            (url, response.code))
