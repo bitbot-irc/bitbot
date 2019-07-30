d2155eb20408dcbf4ac3cc1cdca5282a2d211cd8
jesopo <github@lolnerd.net>
2019-07-30 14:40:07 +0100

    .eat()ing privmsg/notice send should be high priority

--- modules/ircv3_echo_message.py
+++ modules/ircv3_echo_message.py
@@ -4,8 +4,8 @@ CAP = utils.irc.Capability("echo-message", depends_on=["labeled-response"])
 
 @utils.export("cap", CAP)
 class Module(ModuleManager.BaseModule):
-    @utils.hook("raw.send.privmsg", priority=EventManager.PRIORITY_LOW)
-    @utils.hook("raw.send.notice", priority=EventManager.PRIORITY_LOW)
+    @utils.hook("raw.send.privmsg", priority=EventManager.PRIORITY_HIGH)
+    @utils.hook("raw.send.notice", priority=EventManager.PRIORITY_HIGH)
     def send_message(self, event):
         if event["server"].has_capability(CAP):
             event.eat()
