748008cf360616752375f72e619f741a3b224a6a
jesopo <github@lolnerd.net>
2019-09-11 17:54:55 +0100

    switch to using IRCBot.http_client() in title.py

--- modules/title.py
+++ modules/title.py
@@ -26,7 +26,7 @@ class Module(ModuleManager.BaseModule):
             return None
 
         try:
-            page = utils.http.request(url, parse=True)
+            page = self.bot.http_client().request(url, parse=True)
         except utils.http.HTTPWrongContentTypeException:
             return None
         except Exception as e:
