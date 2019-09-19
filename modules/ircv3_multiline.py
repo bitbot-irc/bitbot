c7409c91be3d7b4232c39c219ac596f564cb0337
jesopo <github@lolnerd.net>
2019-09-19 16:24:53 +0100

    add missing IRCLine import

--- modules/ircv3_multiline.py
+++ modules/ircv3_multiline.py
@@ -1,4 +1,4 @@
-from src import ModuleManager, utils
+from src import IRCLine, ModuleManager, utils
 
 CAP = utils.irc.Capability(None, "draft/multiline", alias="multiline")
 BATCH = utils.irc.BatchType(None, "draft/multiline")
