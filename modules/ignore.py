3c1a313ef2470048c909324a98debd6777503be0
jesopo <github@lolnerd.net>
2019-09-26 12:15:14 +0100

    'require-mode'/'require-access' -> 'require_mode'/'require_access'
    closes #161

--- modules/ignore.py
+++ modules/ignore.py
@@ -96,8 +96,8 @@ class Module(ModuleManager.BaseModule):
     @utils.kwarg("min_args", 1)
     @utils.kwarg("usage", "<nickname>")
     @utils.kwarg("permission", "cignore")
-    @utils.kwarg("require-mode", "o")
-    @utils.kwarg("require-access", "cignore")
+    @utils.kwarg("require_mode", "o")
+    @utils.kwarg("require_access", "cignore")
     def cignore(self, event):
         remove = event["command"] == "cunignore"
 
