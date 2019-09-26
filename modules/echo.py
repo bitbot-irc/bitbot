1da8e15b785450d6fc3d35cf56f4dac3519c63bf
jesopo <github@lolnerd.net>
2019-09-26 11:56:37 +0100

    !echo should have a min_args kwarg

--- modules/echo.py
+++ modules/echo.py
@@ -4,5 +4,6 @@ from src import ModuleManager, utils
 
 class Module(ModuleManager.BaseModule):
     @utils.hook("received.command.echo")
+    @utils.kwarg("min_args", 1)
     def echo(self, event):
         event["stdout"].write(event["args"])
