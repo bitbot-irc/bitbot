7db17c09628b8f30b468e47dd78aa51f6b6c7e33
jesopo <github@lolnerd.net>
2019-09-26 13:44:38 +0100

    add utils.parse.try_int() because .isdigit() isnt good enough

--- src/utils/parse.py
+++ src/utils/parse.py
@@ -73,3 +73,9 @@ def keyvalue(s: str, delimiter: str=" "
         else:
             items[key] = None
     return items
+
+def try_int(s: str) -> typing.Optional[int]:
+    try:
+        return int(s)
+    except ValueError:
+        return None
