bc631119842bef5444160b099c99f6a9f0d664d1
jesopo <github@lolnerd.net>
2019-09-27 16:18:49 +0100

    add !msg

--- modules/echo.py
+++ modules/echo.py
@@ -13,3 +13,12 @@ class Module(ModuleManager.BaseModule):
     @utils.kwarg("expect_output", False)
     def action(self, event):
         event["target"].send_message("\x01ACTION %s\x01" % event["args"])
+
+    @utils.hook("received.command.msg")
+    @utils.kwarg("min_args", 2)
+    @utils.kwarg("permission", "say")
+    @utils.kwarg("remove_empty", False)
+    @utils.kwarg("help", "Send a message to a target")
+    def msg(self, event):
+        event["server"].send_message(event["args_split"][0],
+            " ".join(event["args_split"][1:]))
