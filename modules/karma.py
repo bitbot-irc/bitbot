a75802129fed28005c7faa4e5bbe1ff05ba64ac7
jesopo <github@lolnerd.net>
2019-09-30 15:36:13 +0100

    allow whitespace between karma target and ++/--

--- modules/karma.py
+++ modules/karma.py
@@ -8,7 +8,7 @@ from src import EventManager, ModuleManager, utils
 WORD_STOP = [",", ":"]
 KARMA_DELAY_SECONDS = 3
 
-REGEX_KARMA = re.compile(r'(?:\(([^)]+)\)|(\S+))(\+\+|--)(\s+|$)')
+REGEX_KARMA = re.compile(r'(?:\(([^)]+)\)|(\S+))\s*(\+\+|--)(\s+|$)')
 
 @utils.export("channelset", utils.BoolSetting("karma-pattern",
     "Enable/disable parsing ++/-- karma format"))
