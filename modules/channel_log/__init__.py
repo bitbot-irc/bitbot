a8113488aa9d7ff8997f157a6867282b5515240c
jesopo <github@lolnerd.net>
2019-10-31 15:36:22 +0000

    include CHGHOST lines in channel_log logs

--- modules/channel_log/__init__.py
+++ modules/channel_log/__init__.py
@@ -31,6 +31,7 @@ class Module(ModuleManager.BaseModule):
     @utils.hook("formatted.kick")
     @utils.hook("formatted.quit")
     @utils.hook("formatted.rename")
+    @utils.hook("formatted.chghost")
     def on_formatted(self, event):
         if event["channel"]:
             self._log(event, event["channel"])
