718a026beb8b1bd208083fdd59acc9fc6f2c8122
jesopo <github@lolnerd.net>
2019-11-15 10:57:29 +0000

    don't respond to !botlist automatically

--- modules/botlist.py
+++ modules/botlist.py
@@ -3,8 +3,11 @@ from src import ModuleManager, utils
 COMMANDS = ["!botlist", "!rollcall"]
 MESSAGE = "Hi! I'm BitBot (https://git.io/bitbot) "
 
+@utils.export("botset", utils.BoolSetting("botlist",
+    "Whether or not I should respond to !botlist commands"))
 class Module(ModuleManager.BaseModule):
     @utils.hook("received.message.channel")
     def channel_message(self, event):
-        if event["message"].strip() in COMMANDS:
+        if (event["message"].strip() in COMMANDS and
+                self.bot.get_setting("botlist", False)):
             event["channel"].send_message(MESSAGE)
