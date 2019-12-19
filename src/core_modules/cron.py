a2ac26c07209c2f038c9ac9632924dbe2c68ac95
jesopo <github@lolnerd.net>
2019-12-19 14:02:55 +0000

    use +=timedelta(minutes=1) when calculating next minute
    closes #223

--- src/core_modules/cron.py
+++ src/core_modules/cron.py
@@ -4,7 +4,8 @@ from src import ModuleManager, utils
 class Module(ModuleManager.BaseModule):
     def on_load(self):
         now = datetime.datetime.utcnow()
-        next_minute = now.replace(minute=now.minute+1, second=0, microsecond=0)
+        next_minute = now.replace(second=0, microsecond=0)
+        next_minute += datetime.timedelta(minutes=1)
         until = time.time()+((next_minute-now).total_seconds())
         self.timers.add("cron", self._minute, 60, until)
 
