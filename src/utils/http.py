366a7bf326c078eb935196195908c1ba90f13539
jesopo <github@lolnerd.net>
2020-01-06 17:17:49 +0000

    Revert "explicitly .close() task in http.request_many()"
    This reverts commit 7478d62b77a7c7637000670d1c2ed22b1b403745.

--- src/utils/http.py
+++ src/utils/http.py
@@ -258,7 +258,6 @@ def request_many(requests: typing.List[Request]) -> typing.Dict[str, Response]:
     task = asyncio.wait(awaits, loop=loop, timeout=5)
     loop.run_until_complete(task)
     loop.close()
-    task.close()
 
     return responses
 
