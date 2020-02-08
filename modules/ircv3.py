632d4eb7b0a004cfdf52c1b5de2b99f485f829ef
jesopo <github@lolnerd.net>
2020-02-08 19:00:22 +0000

    show server aliases in !specsup

--- modules/ircv3.py
+++ modules/ircv3.py
@@ -18,8 +18,8 @@ class Module(ModuleManager.BaseModule):
                     port = str(server.connection_params.port)
                     if server.connection_params.tls:
                         port = "+%s" % port
-                    supporting_servers.append("%s:%s (%s)" % (
-                        server.connection_params.hostname, port,
+                    supporting_servers.append("%s (%s:%s %s)" % (
+                        str(server), server.connection_params.hostname, port,
                         server.version))
         if supporting_servers:
             event["stdout"].write("%s: %s" % (spec,
