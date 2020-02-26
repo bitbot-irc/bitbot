f82e3536f0aa8adc8ae5f8855de57697bdc8043f
jesopo <github@lolnerd.net>
2020-02-26 17:30:36 +0000

    move bot_channel.py to core_modules, default to no bot channel

--- src/core_modules/bot_channel.py
+++ src/core_modules/bot_channel.py
@@ -8,5 +8,6 @@ class Module(ModuleManager.BaseModule):
     @utils.hook("received.001")
     def do_join(self, event):
         bot_channel = event["server"].get_setting("bot-channel",
-            self.bot.config.get("bot-channel", "#bitbot"))
-        event["server"].send_join(bot_channel)
+            self.bot.config.get("bot-channel", None))
+        if not bot_channel == None:
+            event["server"].send_join(bot_channel)
