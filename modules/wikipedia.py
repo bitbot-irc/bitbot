540cb766b91ee85030172503a62384ea729ebacc
David Schultz <ek311@outlook.com>
2020-10-08 12:54:24 -0500

    make wikipedia module work with disambig

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -5,6 +5,33 @@ from src import ModuleManager, utils
 URL_WIKIPEDIA = "https://en.wikipedia.org/w/api.php"
 
 class Module(ModuleManager.BaseModule):
+    def listify(self, items):
+        if type(items) != list:
+           items = list(items)
+        return len(items) > 1 and ', '.join(items[:-1]) + ', or ' + items[-1] or items and items[0] or ''
+    
+    def disambig(self, title):
+        api = utils.http.request(URL_WIKIPEDIA, get_params={
+            "action": "parse", "format": "json", "page": page, "prop": "wikitext"}).json()
+        if api:
+            text = api['parse']['wikitext']['*']
+            links = re.findall('\* \[\[(.*)\]\]', text)
+            disambigs = []
+            if links:
+                for link in links:
+                    # parse through the wikitext adventure
+                    if '|' in link:
+                        d = link.split('|')[1]
+                    else:
+                        d = link
+                    d = d.replace('\'', '').replace('\'', '').replace('"', '')
+                    disambigs.append(d)
+            else:
+                return 'Unable to parse disambiguation page. You may view the page at'
+            if len(disambigs) > 15:
+                return 'Sorry, but this page is too ambiguous. You may view the page at'
+            else:
+                return '%s could mean %s -' % (title, self.listify(disambigs))
     @utils.hook("received.command.wi", alias_of="wiki")
     @utils.hook("received.command.wiki", alias_of="wikipedia")
     @utils.hook("received.command.wikipedia")
@@ -24,7 +51,8 @@ class Module(ModuleManager.BaseModule):
                 title = article["title"]
                 info = utils.parse.line_normalise(article["extract"])
                 url = article["fullurl"]
-
+                if 'may refer to' in info:
+                    event["stdout.write("%s %s" % (self.disambig(title), url)
                 event["stdout"].write("%s: %s - %s" % (title, info, url))
             else:
                 event["stderr"].write("No results found")
