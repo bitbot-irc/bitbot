9ee569a960071729d716eb336f1f48effccd12bd
David Schultz <ek311@outlook.com>
2020-10-08 18:05:55 -0500

    +autolink feature

--- modules/wikipedia.py
+++ modules/wikipedia.py
@@ -13,6 +13,9 @@ URL_WIKIPEDIA = "https://$lang.wikipedia.org/w/api.php"
     "Choose which language to use for Wikipedia",
     example="en"))
 
+@utils.export("channelset", utils.BoolSetting("wikipedia-autolink",
+    "Auto-translate to wiki-links"))
+
 class Module(ModuleManager.BaseModule):
     def listify(self, items):
         if type(items) != list:
@@ -44,6 +47,38 @@ class Module(ModuleManager.BaseModule):
                 return 'Sorry, but this page is too ambiguous. You may view the page at'
             else:
                 return '%s could mean %s -' % (title, self.listify(disambigs))
+    
+    
+    @utils.hook("received.message.channel")
+    def handle_chanmsg(self, event):
+        if not event["channel"].get_setting("wikipedia-autolink", False):
+            return
+        wikilink = re.search("\[\[(.*)\]\]", event["message"])
+        if wikilink:
+            page = wikilink.group(1)
+        page = utils.http.request(URL_WIKIPEDIA.replace('$lang', event["channel"].get_setting("wikipedia-lang", "en")), get_params={
+            "action": "query", "prop": "extracts|info", "inprop": "url",
+            "titles": page, "exintro": "", "explaintext": "",
+            "exchars": "500", "redirects": "", "format": "json"}).json()
+
+        if page:
+            pages = page["query"]["pages"]
+            article = list(pages.items())[0][1]
+            if not "missing" in article:
+                title, info = article["title"], article["extract"]
+                title = article["title"]
+                info = utils.parse.line_normalise(article["extract"])
+                url = article["fullurl"]
+                if 'may refer to' in info:
+                    event["channel"].send_message("%s %s" % (self.disambig(title, event), url))
+                else:
+                    event["channel"].send_message("%s: %s - %s" % (title, info, url))
+            else:
+                event["channel"].send_message("No results found")
+        else:
+            raise utils.EventResultsError()
+    
+    
     @utils.hook("received.command.wi", alias_of="wiki")
     @utils.hook("received.command.wiki", alias_of="wikipedia")
     @utils.hook("received.command.wikipedia")
