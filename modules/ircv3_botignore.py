07fcbd6c9e61caaeffb073b759b33b769b43621a
David Schultz <me@zpld.me>
2021-06-11 23:52:13 -0500

    fully support draft/bot spec

--- modules/ircv3_botignore.py
+++ modules/ircv3_botignore.py
@@ -1,11 +1,22 @@
 from src import EventManager, ModuleManager, utils
 
-TAG = utils.irc.MessageTag(None, "inspircd.org/bot")
+TAGS = {
+    utils.irc.MessageTag(None, "inspircd.org/bot"),
+    utils.irc.MessageTag(None, "draft/bot")
+}
 
 class Module(ModuleManager.BaseModule):
+    @utils.hook("received.376")
+    @utils.hook("received.422")
+    def botmode(self, event):
+        if "BOT" in event["server"].isupport:
+            botmode = event["server"].isupport["BOT"]
+            event["server"].send_raw("MODE %s +%s" % (event["server"].nickname, botmode))
+
     @utils.hook("received.message.private")
     @utils.hook("received.message.channel")
     @utils.kwarg("priority", EventManager.PRIORITY_HIGH)
     def message(self, event):
-        if TAG.present(event["tags"]):
-            event.eat()
+        for tag in TAGS:
+            if tag.present(event["tags"]):
+                event.eat()
