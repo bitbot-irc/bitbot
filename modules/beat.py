b1699a8c5e41048b7c3814355a6b6468faa6649e
khuxkm fbexl <khuxkm@ttm.sh>
2021-07-02 03:32:51 +0000

    Update modules/beat.py to follow PEP8 style

--- modules/beat.py
+++ modules/beat.py
@@ -2,14 +2,15 @@ from src import ModuleManager, utils
 import datetime
 
 class Module(ModuleManager.BaseModule):
-	_name = "beat"
+    _name = "beat"
 
-	@utils.hook("received.command.beat")
-	@utils.kwarg("help","Gives the current Swatch Internet Time (aka .beat time)")
-	def beat(self, event):
-		now = datetime.datetime.utcnow()
-		hour = (now.hour+1)%24
-		minutes = now.minute
-		seconds = now.second
-		beat_time = ((hours*3600)+(minutes*60)+seconds)/86.4
-		event["stdout"].write("The current time is @%0.2f." % (beat_time))
+
+    @utils.hook("received.command.beat")
+    @utils.kwarg("help","Gives the current .beat time")
+    def beat(self, event):
+        now = datetime.datetime.utcnow()
+        hour = (now.hour+1)%24
+        minutes = now.minute
+        seconds = now.second
+        beat_time = ((hours*3600)+(minutes*60)+seconds)/86.4
+        event["stdout"].write("The current time is @%0.2f." % (beat_time))
